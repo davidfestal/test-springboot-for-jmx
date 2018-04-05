0c4c56c7a5f1d067d14d454e131d4e182c93b47a
David Festal <dfestal@redhat.com>
2018-04-05 16:03:39 +0000

    small change

--- src/main/java/io/openshift/booster/service/GreetingProperties.java
+++ src/main/java/io/openshift/booster/service/GreetingProperties.java
@@ -23,7 +23,7 @@ import org.springframework.stereotype.Component;
 @ConfigurationProperties("greeting")
 public class GreetingProperties {
 
-    private String message = "Hello, %s!";
+    private String message = "Hello, %s!, it's David";
 
     public String getMessage() {
         return message;
